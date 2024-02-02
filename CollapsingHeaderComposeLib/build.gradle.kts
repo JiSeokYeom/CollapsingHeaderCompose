a50b9231c89a6f14e9c8a8fcaf7525cf0c86644f
Ji Seok <ygs01033@naver.com>
2024-02-02 14:02:09 +0900

    fix: build error

--- CollapsingHeaderComposeLib/build.gradle.kts
+++ CollapsingHeaderComposeLib/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
-val compose_ui_version = "1.2.0"
+val compose_ui_version = "1.6.1"
 
 android {
     namespace = "com.collapsingheadercompose"
@@ -32,6 +32,10 @@ android {
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
 }
 
 dependencies {
@@ -44,10 +48,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.ui:ui:$compose_ui_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_ui_version")
-    implementation("androidx.compose.materialcompose_ui_version:material:1.2.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
 }
 
 group = "com.github.JiSeokYeom"
