7e00db8d398f2d408879767770cb94bc5eea02ce
yeomjiseok <ygs01033@naver.com>
2024-02-04 00:43:56 +0900

    fix: build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ buildscript {
     dependencies {
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+    }
+
 }
 
 plugins {
@@ -13,3 +18,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.2.0" apply false
 }
+
